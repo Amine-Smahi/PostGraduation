8f2060713ffbabb9addc306d1009f0213c1c8472
Amine-Smahi <mohammed.amine.smahi@gmail.com>
2018-05-30 20:44:34 +0100

    Add urls

---
+++ Administration/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from . import views
+
+router = DefaultRouter()
+
+router.register(r'inscription', views.RecipeViewSet)
+router.register(r'reinscription', views.CourseViewSet)
+router.register(r'recourt', views.CuisineViewSet)
+router.register(r'emploitDuTemp', views.CategoryViewSet)
+router.register(r'Sujet', views.CategoryViewSet)
+router.register(r'listEtudiant', views.CategoryViewSet)
+router.register(r'pvSoutenance', views.CategoryViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
