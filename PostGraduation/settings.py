c578b6463ae41f33eda9f4aeb4c7101c9d6cdb57
Amine-Smahi <mohammed.amine.smahi@gmail.com>
2018-05-30 20:10:41 +0100

    Configure project

--- PostGraduation/settings.py
+++ PostGraduation/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'Administration',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
+}
--- PostGraduation/urls.py
+++ PostGraduation/urls.py
@@ -18,4 +18,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('Administration/', include('Administration.urls')),
 ]
